1b19832b6c8d302c03b4b42c5c1a69394c77179a
goloobab <goloobab@gmail.com>
2023-02-07 21:10:26 +0000

    Added info in the about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -38,10 +38,11 @@ function About() {
                             </div>
                         </div>
                         <p>
-                            Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et
-                            ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
-                            Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque.
-                            Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
+                        I am a junior front-end developer with a passion for creating user-friendly websites and applications. Proficient in
+                         HTML, CSS, JavaScript, and React, I bring a creative approach to development and a strong attention to detail. 
+                         I have worked on multiple projects, collaborating with teams and delivering high-quality results. Continuously
+                          learning and eager for new challenges, I am excited to grow as a front-end developer."
+
                         </p>
                         <div className="text-center"><a href="files/Resume.pdf" className="btn" download>Download CV</a></div>
                     </div>
