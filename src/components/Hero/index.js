2c9a2ca5fa1eb40fe7695ad559c442ec9a5f24f8
goloobab <goloobab@gmail.com>
2023-02-07 21:48:43 +0000

    added git hub icon

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -17,7 +17,7 @@ function Hero() {
                     />
                 </p>
                 <div className="social-links">
-                    <a href="#" className="twitter"><i className="bx bxl-twitter"></i></a>
+                    <a href="#" className="twitter"><i className="bx bxl-github"></i></a>
                     <a href="#" className="google-plus"><i className="bx bxl-skype"></i></a>
                     <a href="#" className="linkedin"><i className="bx bxl-linkedin"></i></a>
                 </div>
